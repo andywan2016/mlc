a874a59f540ea0991b07b1966994e5840e9352cd
andywan2016 <zw2327@columbia.edu>
2017-11-01 14:57:16 -0400

    similation done

--- src/simulation_class.py
+++ src/simulation_class.py
@@ -1,1 +1,22 @@
-from mlc_class import 
+import random
+from mlc_class import life_table_array,MLC
+
+
+
+def simulation(mlc_obj,start_yr,yr_steps):
+   for i in range(yr_steps):
+      rand_num=random.uniform(0,1)	   
+      qx=mlc_obj.qx(start_yr+i)
+      if rand_num<qx:
+         return [start_yr+i,'dead']
+      else:
+	 continue      
+   return [start_yr+yr_steps,'survive']
+   
+
+if __name__=='__main__':
+   life_table=life_table_array()
+   mlc=MLC(30,life_table)
+   
+   for i in range(20):
+      print(simulation(mlc,30,20))
